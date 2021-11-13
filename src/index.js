71ae1555640d63303a233d3552330e78c393295b
Jérémie Bardon <jerembardon@gmail.com>
2021-11-13 14:41:57 -0500

    Modify index

--- src/index.js
+++ src/index.js
@@ -4,19 +4,23 @@ const resolvers = require('./resolvers');
 
 const TrackAPI = require('./datasources/track-api');
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => {
-    return {
-      trackAPI: new TrackAPI(),
-    };
-  },
-});
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI(),
+      };
+    },
+  });
 
-const { url, port } = await server.listen({ port: process.env.PORT || 4000  });
-console.log(`
-  🚀  Server is running!
-  🔉  Listening on port ${port}
-  📭  Query at ${url}
-`);
+  const { url, port } = await server.listen();
+  console.log(`
+      🚀  Server is running
+      🔉  Listening on port ${port}
+      📭  Query at ${url}
+    `);
+}
+
+startApolloServer(typeDefs, resolvers);
